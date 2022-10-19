1e0164b0e09271f4de92d3e3919bf735195cf6e2
Tyler Jefferson <tylerrjefferson00@gmail.com>
2022-10-19 17:16:01 -0400

    can now add new customers

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { getAllCustomers, getCustomerById } from "./src/customers.js";
+import { addNewCustomer, getAllCustomers, getCustomerById } from "./src/customers.js";
 
 const app = express();
+app.use(express.json());
 
 app.get("/customers", getAllCustomers);
 app.get("/customers/:customerId", getCustomerById)
+app.post("/customers", addNewCustomer)
 
 app.listen(3030, () => console.log("Listening on http://localhost:3030 ..."));
--- src/customers.js
+++ src/customers.js
@@ -3,7 +3,7 @@ import { creds } from "../creds.js";
 
 const { Pool } = pg;
 
-export async function getAllCustomers (req, res) {
+export async function getAllCustomers(req, res) {
   const pool = new Pool(creds)
   const customers = await pool.query("SELECT * FROM customers")
     .catch(err => res.status(500).send(err))
@@ -11,11 +11,30 @@ export async function getAllCustomers (req, res) {
   pool.end()
 }
 
-export async function getCustomerById (req, res) {
+export async function getCustomerById(req, res) {
   const { customerId } = req.params
+  // if(!customerId || customerId != Number(customerId)) {
+  //   res.status(401).send({ message: 'Invalid customer' })
+  //   return
+  // }
   const pool = new Pool(creds)
   const customers = await pool.query(`SELECT * FROM customers WHERE customer_id=${customerId}`)
     .catch(err => res.status(500).send(err))
   res.send(customers.rows)
+  pool.end()
+}
+
+export async function addNewCustomer(req, res) {
+  const { first_name, last_name, phone, email } = req.body
+  const pool = new Pool(creds)
+  const query = `INSERT INTO customers (first_name, last_name, email, phone)
+    VALUES ('${first_name}', '${last_name}', '${email}', '${phone}')`
+  await pool.query(query)
+    .catch(err => {
+      console.log(err)
+      res.status(500).send(err)
+      return
+    })
+  res.status(201).send({ message: "Customer successfully created" })
   pool.end()
 }
